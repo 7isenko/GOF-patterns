385dcb570ef5b056f61c6f64de53e8cab6e1a13d
7isenko <7isenko.max@gmail.com>
2020-05-07 15:22:08 +0300

    +Singleton

---
+++ Creational/Singleton/src/Singleton.java
@@ -0,0 +1,18 @@
+public class Singleton {
+    // Хорошая реализация для мультипоточного приложения .
+    // Плохо работает в версиях Java 1.4 и раньше!
+    private volatile static Singleton uniqueInstance;
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        if (uniqueInstance == null) {
+            synchronized (Singleton.class) {
+                if (uniqueInstance == null)
+                    uniqueInstance = new Singleton();
+            }
+        }
+        return uniqueInstance;
+    }
+}
---
+++ Creational/Singleton/src/SingletonWithoutThreads.java
@@ -0,0 +1,13 @@
+public class SingletonWithoutThreads {
+    // Если в вашем приложении нет потоков
+    private static SingletonWithoutThreads uniqueInstance;
+
+    private SingletonWithoutThreads() {
+    }
+
+    public static SingletonWithoutThreads getInstance() {
+        if (uniqueInstance == null)
+            uniqueInstance = new SingletonWithoutThreads();
+        return uniqueInstance;
+    }
+}
